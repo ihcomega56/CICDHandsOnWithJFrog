9af9ae99f0d1bd4b1f9b91d2c1ae47f71eaad7f3
ihcomega56 <ihcomega56@gmail.com>
2021-12-09 02:29:27 +0900

    Start from here

--- src/main/java/com/example/demoapp/model/Comment.java
+++ src/main/java/com/example/demoapp/model/Comment.java
@@ -3,15 +3,30 @@ package com.example.demoapp.model;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * 発言を管理するクラス
+ */
 public class Comment {
     public Comment(long id, String text) {
         this.text = text;
     }
 
+    /**
+     * 発言をそっくりそのまま返す
+     * @return 元の発言
+     */
     public String echo() {
-        return text;
+        // TODO: テストが通るようコードを修正する
+        return "勝手な発言";
+        // ↑の1行を以下のコードに置き換えればOK
+        // return text;
     }
 
+    /**
+     * 発言を指定した回数繰り返す
+     * @param time 回数
+     * @return 回数分繰り返された発言
+     */
     public String repeat(int time) {
         return IntStream.range(0, time).mapToObj(i -> text).collect(Collectors.joining());
     }
